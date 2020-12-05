58152e1b253cf696ffaea3eb5e53e6f889e19b59
Cadore-ac <arthur.cmb2020@gmail.com>
2020-12-05 02:18:37 +0000

    1

---
+++ fase2.js
@@ -0,0 +1,196 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 300 },
+            debug: false,
+        },
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update,
+    },
+};
+
+var player;
+var player2;
+var stars;
+var platforms;
+var cursors;
+var score = 0;
+var scoreText;
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image("sky", "assets/plano de fundo.png");
+    this.load.image("ground", "assets/plataformagelo.png");
+    this.load.image("star", "assets/star.png");
+    this.load.image("bomb", "assets/bomb.png");
+    this.load.spritesheet("dude", "assets/dude.png", {
+        frameWidth: 32,
+        frameHeight: 48,
+    });
+    this.load.spritesheet("dude2", "assets/dude.png", {
+        frameWidth: 32,
+        frameHeight: 48,
+    });
+}
+
+function create() {
+    this.add.image(400, 300, "sky");
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(64, 582, "ground");
+    platforms.create(256, 582, "ground");
+    platforms.create(192, 582, "ground");
+    platforms.create(384, 582, "ground");
+    platforms.create(512, 582, "ground");
+    platforms.create(640, 582, "ground");
+    platforms.create(768, 582, "ground");
+
+    platforms.create(64, 18, "ground");
+    platforms.create(256, 18, "ground");
+    platforms.create(192, 18, "ground");
+    platforms.create(384, 18, "ground");
+    platforms.create(512, 18, "ground");
+    platforms.create(640, 18, "ground");
+    platforms.create(768, 18, "ground");
+
+    platforms.create(96, 166, "ground");
+    platforms.create(224, 166, "ground");
+
+    platforms.create(480, 166, "ground");
+    platforms.create(544, 166, "ground");
+
+    platforms.create(96, 262, "ground");
+    platforms.create(224, 262, "ground");
+    platforms.create(352, 262, "ground");
+    platforms.create(480, 262, "ground");
+    platforms.create(544, 262, "ground");
+
+    platforms.create(768, 390, "ground");
+    platforms.create(640, 390, "ground");
+    platforms.create(512, 390, "ground");
+    platforms.create(384, 390, "ground");
+
+    platforms.create(96, 486, "ground");
+    platforms.create(384, 486, "ground");
+    platforms.create(768, 486, "ground");
+
+
+
+    player = this.physics.add.sprite(128, 500, "dude");
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: "left",
+        frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1,
+    });
+
+    this.anims.create({
+        key: "turn",
+        frames: [{ key: "dude", frame: 4 }],
+        frameRate: 20,
+    });
+
+    this.anims.create({
+        key: "right",
+        frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1,
+    });
+
+    player2 = this.physics.add.sprite(128, 400, "dude2");
+
+    player2.setBounce(0.2);
+    player2.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: "left",
+        frames: this.anims.generateFrameNumbers("dude2", { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1,
+    });
+
+    this.anims.create({
+        key: "turn",
+        frames: [{ key: "dude2", frame: 4 }],
+        frameRate: 20,
+    });
+
+    this.anims.create({
+        key: "right",
+        frames: this.anims.generateFrameNumbers("dude2", { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1,
+    });
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    stars = this.physics.add.group({
+        key: "star",
+        repeat: 11,
+        setXY: { x: 12, y: 0, stepX: 70 },
+    });
+
+
+
+    scoreText = this.add.text(16, 16, "score: 0", {
+        fontSize: "32px",
+        fill: "#000",
+    });
+
+    this.physics.add.collider(player, platforms);
+    this.physics.add.collider(stars, platforms);
+    this.physics.add.overlap(player, stars, collectStar, null, this);
+
+    this.physics.add.collider(player2, platforms);
+    this.physics.add.overlap(player2, stars, collectStar, null, this);
+}
+
+function update() {
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+        player2.setVelocityX(-160);
+
+        player.anims.play("left", true);
+        player2.anims.play("left", true);
+
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+        player2.setVelocityX(160);
+
+        player.anims.play("right", true);
+        player2.anims.play("right", true);
+    } else {
+        player.setVelocityX(0);
+        player2.setVelocityX(0);
+
+        player.anims.play("turn");
+        player2.anims.play("turn");
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-300);
+        player2.setVelocityY(-300);
+    }
+}
+
+
+
+function collectStar(player, star) {
+    star.disableBody(true, true);
+
+    score += 10;
+    scoreText.setText("Score: " + score);
+}
