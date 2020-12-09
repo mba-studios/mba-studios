5c73b4def7d3bcaae7022aa55e7de52e50fd220a
Cadore-ac <arthur.cmb2020@gmail.com>
2020-12-09 02:52:12 +0000

    1

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 var config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: { y: 300 },
-      debug: false,
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 300 },
+            debug: false,
+        },
     },
-  },
-  scene: {
-    preload: preload,
-    create: create,
-    update: update,
-  },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update,
+    },
 };
 
 var player;
@@ -23,196 +23,244 @@ var platforms;
 var cursors;
 var score = 0;
 var scoreText;
+var water;
 
 var game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image("sky", "assets/plano de fundo.png");
-  this.load.image("ground", "assets/plataformagelo.png");
-  this.load.image("wall", "assets/plataformagelovertical.png");
-  this.load.image("slab", "assets/plataformagelohorizontalmeiobloco.png");
-  this.load.image("star", "assets/star.png");
-  this.load.image("bomb", "assets/bomb.png");
-  this.load.spritesheet("dude", "assets/dino1.png", {
-    frameWidth: 24,
-    frameHeight: 24,
-  });
-  this.load.spritesheet("dude2", "assets/dino1.png", {
-    frameWidth: 24,
-    frameHeight: 24,
-  });
+    this.load.image("sky", "assets/plano de fundo.png");
+    this.load.image("ground", "assets/plataformagelo.png");
+    this.load.image("wall", "assets/plataformagelovertical.png");
+    this.load.image("slab", "assets/plataformagelohorizontalmeiobloco.png");
+    this.load.image("star", "assets/star.png");
+    this.load.image("bomb", "assets/bomb.png");
+    this.load.spritesheet("dude", "assets/dino1.png", {
+        frameWidth: 24,
+        frameHeight: 24,
+    });
+
+    this.load.spritesheet("water", "assets/líquidoágua.png", {
+        frameWidth: 128,
+        frameHeight: 16,
+    });
+    this.load.spritesheet("dude2", "assets/dino1.png", {
+        frameWidth: 24,
+        frameHeight: 24,
+    });
 }
 
 function create() {
-  this.add.image(400, 300, "sky");
+    this.add.image(400, 300, "sky");
 
-  platforms = this.physics.add.staticGroup();
+    platforms = this.physics.add.staticGroup();
+    water = this.physics.add.staticGroup();
 
-  platforms.create(64, 582, "ground");
-  platforms.create(256, 582, "ground");
-  platforms.create(192, 582, "ground");
-  platforms.create(384, 590, "slab");
-  platforms.create(512, 582, "ground");
-  platforms.create(640, 582, "ground");
-  platforms.create(768, 582, "ground");
+    water.create(224, 390, "water");
 
-  platforms.create(64, 8, "ground");
-  platforms.create(256, 8, "ground");
-  platforms.create(192, 8, "ground");
-  platforms.create(384, 8, "ground");
-  platforms.create(512, 8, "ground");
-  platforms.create(640, 8, "ground");
-  platforms.create(768, 8, "ground");
+    platforms.create(64, 582, "ground");
+    platforms.create(256, 582, "ground");
+    platforms.create(192, 582, "ground");
+    platforms.create(384, 500, "slab");
+    platforms.create(512, 582, "ground");
+    platforms.create(640, 582, "ground");
+    platforms.create(768, 582, "ground");
 
-  platforms.create(96, 486, "ground");
-  platforms.create(224, 486, "ground");
-  platforms.create(704, 486, "ground");
+    platforms.create(64, 8, "ground");
+    platforms.create(256, 8, "ground");
+    platforms.create(192, 8, "ground");
+    platforms.create(384, 8, "ground");
+    platforms.create(512, 8, "ground");
+    platforms.create(640, 8, "ground");
+    platforms.create(768, 8, "ground");
 
-  platforms.create(96, 390, "ground");
-  platforms.create(224, 398, "slab");
-  platforms.create(352, 390, "ground");
-  platforms.create(480, 390, "ground");
-  platforms.create(544, 390, "ground");
+    platforms.create(96, 486, "ground");
+    platforms.create(224, 486, "ground");
+    platforms.create(704, 486, "ground");
 
-  platforms.create(32, 294, "ground");
+    platforms.create(96, 390, "ground");
+    platforms.create(224, 398, "slab");
+    platforms.create(352, 390, "ground");
+    platforms.create(480, 390, "ground");
+    platforms.create(544, 390, "ground");
 
-  platforms.create(704, 262, "ground");
-  platforms.create(576, 270, "slab");
-  platforms.create(448, 262, "ground");
-  platforms.create(352, 230, "ground");
-  platforms.create(224, 230, "ground");
+    platforms.create(32, 294, "ground");
 
-  platforms.create(704, 166, "ground");
+    platforms.create(704, 262, "ground");
+    platforms.create(576, 270, "slab");
+    platforms.create(448, 262, "ground");
+    platforms.create(352, 230, "ground");
+    platforms.create(224, 230, "ground");
 
-  platforms.create(16, 536, "wall");
-  platforms.create(16, 408, "wall");
-  platforms.create(16, 280, "wall");
-  platforms.create(16, 152, "wall");
-  platforms.create(16, 24, "wall");
+    platforms.create(704, 166, "ground");
 
-  platforms.create(784, 536, "wall");
-  platforms.create(784, 408, "wall");
-  platforms.create(784, 280, "wall");
-  platforms.create(784, 152, "wall");
-  platforms.create(784, 24, "wall");
+    platforms.create(16, 536, "wall");
+    platforms.create(16, 408, "wall");
+    platforms.create(16, 280, "wall");
+    platforms.create(16, 152, "wall");
+    platforms.create(16, 24, "wall");
 
-  player = this.physics.add.sprite(128, 500, "dude");
+    platforms.create(784, 536, "wall");
+    platforms.create(784, 408, "wall");
+    platforms.create(784, 280, "wall");
+    platforms.create(784, 152, "wall");
+    platforms.create(784, 24, "wall");
 
-  player.setBounce(0.2);
-  player.setCollideWorldBounds(true);
+    player = this.physics.add.sprite(128, 500, "dude");
 
-  this.anims.create({
-    key: "left",
-    frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
-    frameRate: 10,
-    repeat: -1,
-  });
 
-  this.anims.create({
-    key: "turn",
-    frames: [{ key: "dude", frame: 1 }],
-    frameRate: 20,
-  });
 
-  this.anims.create({
-    key: "right",
-    frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
-    frameRate: 10,
-    repeat: -1,
-  });
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
 
-  player2 = this.physics.add.sprite(128, 400, "dude2");
+    this.anims.create({
+        key: "left",
+        frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1,
+    });
 
-  player2.setBounce(0.2);
-  player2.setCollideWorldBounds(true);
+    this.anims.create({
+        key: "turn",
+        frames: [{ key: "dude", frame: 1 }],
+        frameRate: 20,
+    });
 
-  this.anims.create({
-    key: "left",
-    frames: this.anims.generateFrameNumbers("dude2", { start: 5, end: 8 }),
-    frameRate: 10,
-    repeat: -1,
-  });
+    this.anims.create({
+        key: "right",
+        frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1,
+    });
 
-  this.anims.create({
-    key: "turn",
-    frames: [{ key: "dude2", frame: 4 }],
-    frameRate: 20,
-  });
+    player2 = this.physics.add.sprite(128, 400, "dude2");
 
-  this.anims.create({
-    key: "right",
-    frames: this.anims.generateFrameNumbers("dude2", { start: 5, end: 8 }),
-    frameRate: 10,
-    repeat: -1,
-  });
+    player2.setBounce(0.2);
+    player2.setCollideWorldBounds(true);
 
-  cursors = this.input.keyboard.createCursorKeys();
+    this.anims.create({
+        key: "left2",
+        frames: this.anims.generateFrameNumbers("dude2", { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1,
+    });
 
-  stars = this.physics.add.group({
-    key: "star",
-    repeat: 11,
-    setXY: {
-      x: 64,
-      y: 350,
+    this.anims.create({
+        key: "turn2",
+        frames: [{ key: "dude2", frame: 1 }],
+        frameRate: 20,
+    });
 
-      x: 128,
-      y: 400,
-    },
-  });
+    this.anims.create({
+        key: "right2",
+        frames: this.anims.generateFrameNumbers("dude2", { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1,
+    });
 
-  scoreText = this.add.text(16, 16, "score: 0", {
-    fontSize: "32px",
-    fill: "#000",
-  });
+    cursors = this.input.keyboard.createCursorKeys();
 
-  this.physics.add.collider(player, platforms);
-  this.physics.add.collider(stars, platforms);
-  this.physics.add.overlap(player, stars, collectStar, null, this);
+    stars = this.physics.add.group({
+        key: "star",
+        repeat: 11,
+        setXY: {
+            x: 64,
+            y: 350,
 
-  this.physics.add.collider(player2, platforms);
-  this.physics.add.overlap(player2, stars, collectStar, null, this);
+            x: 128,
+            y: 400,
+        },
+    });
 
-  this.input.keyboard.on(
-    "keydown_A",
-    function () {
-      player2.setVelocityX(-160);
-      player2.anims.play("left", true);
-    },
-    this
-  );
-  this.input.keyboard.on(
-    "keyup_A",
-    function () {
-      player2.setVelocityX(0);
-      player2.anims.play("left", true);
-    },
-    this
-  );
+    scoreText = this.add.text(16, 16, "score: 0", {
+        fontSize: "32px",
+        fill: "#000",
+    });
+
+    this.physics.add.collider(player, platforms);
+    this.physics.add.collider(stars, platforms);
+    this.physics.add.overlap(player, stars, collectStar, null, this);
+
+    this.physics.add.collider(player2, platforms);
+    this.physics.add.overlap(player2, stars, collectStar, null, this);
+
+    this.input.keyboard.on(
+        "keydown_A",
+        function () {
+            player2.setVelocityX(-160);
+            player2.anims.play("left2", true);
+        },
+        this
+    );
+
+
+
+    this.input.keyboard.on(
+        "keyup_A",
+        function () {
+            player2.setVelocityX(0);
+            player2.anims.play("turn2", true);
+        },
+        this
+    );
+
+
+    this.input.keyboard.on(
+        "keydown_D",
+        function () {
+            player2.setVelocityX(+160);
+            player2.anims.play("right2", true);
+        },
+        this
+    );
+
+    this.input.keyboard.on(
+        "keyup_D",
+        function () {
+            player2.setVelocityX(0);
+            player2.anims.play("turn2", true);
+        },
+        this
+    );
+
+
+
+    this.input.keyboard.on(
+        "keydown_W",
+        function () {
+
+            if (player.body.touching.down == 1) {
+
+                player2.setVelocityY(-300);
+
+            }
+        },
+        this
+    );
 }
 
 function update() {
-  if (cursors.left.isDown) {
-    player.setVelocityX(-160);
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
 
-    player.anims.play("left", true);
-  } else if (cursors.right.isDown) {
-    player.setVelocityX(160);
+        player.anims.play("left", true);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
 
-    player.anims.play("right", true);
-  } else {
-    player.setVelocityX(0);
+        player.anims.play("right", true);
+    } else {
+        player.setVelocityX(0);
 
-    player.anims.play("turn");
-  }
+        player.anims.play("turn");
+    }
 
-  if (cursors.up.isDown && player.body.touching.down) {
-    player.setVelocityY(-300);
-  }
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-300);
+    }
 }
 
 function collectStar(player, star) {
-  star.disableBody(true, true);
+    star.disableBody(true, true);
 
-  score += 10;
-  scoreText.setText("Score: " + score);
+    score += 10;
+    scoreText.setText("Score: " + score);
 }
