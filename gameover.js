34672e45ff7868f4197e18648cce3394feb63019
bruna-s2001 <74878944+bruna-s2001@users.noreply.github.com>
2020-12-11 19:41:03 -0300

    Add files via upload

---
+++ gameover.js
@@ -0,0 +1,15 @@
+const iniciar = document.getElementById("replay");
+
+const home = document.getElementById("menu");
+
+
+var clicou = "ok";
+
+
+iniciar.addEventListener("click", function () {
+    alert(clicou);
+});
+
+home.addEventListener("click", function () {
+    alert(clicou);
+});
---
+++ inicio.js
@@ -0,0 +1,8 @@
+const play = document.getElementById("startGame");
+
+var start = "ok"
+
+play.addEventListener("click", function () {
+    alert(start);
+});
+
