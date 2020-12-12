0d7d25ecd9285ee3ba4eab3de015e6e9edce3701
Cadore-ac <arthur.cmb2020@gmail.com>
2020-12-12 16:16:57 +0000

    12/12

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+import { cena0 } from "../scripts/cena0";
+import { cena1 } from "../scripts/cena1";
+import { cena2 } from "../scripts/cena2";
+import { cena2 } from "../scripts/gameover";
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: "game-container",
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 300 },
+        },
+    },
+    scene: [cena0, cena1, cena2, gameover],
+};
+
+const game = new Phaser.Game(config);
