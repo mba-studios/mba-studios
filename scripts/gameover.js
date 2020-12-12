712f52d6720ec0ef28728ab7e066866813f45e9f
Cadore-ac <arthur.cmb2020@gmail.com>
2020-12-12 16:27:44 +0000

    12/12

--- scripts/gameover.js
+++ scripts/gameover.js
@@ -13,3 +13,36 @@ iniciar.addEventListener("click", function () {
 home.addEventListener("click", function () {
     alert(clicou);
 });
+
+//------------------------------//
+
+import { cena1 } from "../scripts/index.js";
+
+var cena2 = new Phaser.Scene("Cena 2");
+
+cena2.preload = function () {
+    // Imagem de fundo
+    this.load.image("perdeu", "../assets/gameovertela.png");
+};
+
+iniciar.on(
+    "pointerdown",
+    function () {
+        this.scene.start(cena1);
+    },
+    this
+);
+
+home.on(
+    "pointerdown",
+    function () {
+        this.scene.start(cena0);
+    },
+    this
+);
+
+
+cena2.update = function () { };
+
+// Exportar a cena
+export { cena2 };
