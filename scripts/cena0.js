d89913d71d970a4af2da702e089f2a9b217ab58c
Cadore-ac <arthur.cmb2020@gmail.com>
2020-12-13 03:52:47 +0000

    12/12

--- scripts/cena0.js
+++ scripts/cena0.js
@@ -1,31 +1,24 @@
-const play = document.getElementById("startGame");
-
-var start = "ok"
-
-play.addEventListener("click", function () {
-    alert(start);
-});
-
-//---------------------------//
-
-import { cena3 } from "../scripts/index.js";
+import { cena3 } from "./gameover.js";
 
 var cena0 = new Phaser.Scene("Cena 0");
 
 cena0.preload = function () {
-    // Imagem de fundo
-    this.load.image("cadeado", "../assets/tela-de-inicio-td.png");
-
-    play.on(
-        "pointerdown",
-        function () {
-            this.scene.start(cena3);
-        },
-        this
-    );
+  // Imagem de fundo
+  this.load.image("cadeado", "../assets/tela-de-inicio-td.png");
 };
 
-cena0.update = function () { };
+cena0.create = function () {
+   var button = this.add.image(400, 300, "cadeado", 0).setInteractive();
+   button.on(
+     "pointerdown",
+     function () {
+       this.scene.start(cena3);
+     },
+     this
+   );
+};
+
+cena0.update = function () {};
 
 // Exportar a cena
 export { cena0 };
--- scripts/gameover.js
+++ scripts/gameover.js
@@ -1,48 +1,24 @@
-const iniciar = document.getElementById("replay");
-
-const home = document.getElementById("menu");
-
-
-var clicou = "ok";
-
-
-iniciar.addEventListener("click", function () {
-    alert(clicou);
-});
-
-home.addEventListener("click", function () {
-    alert(clicou);
-});
-
-//------------------------------//
-
-import { cena1 } from "../scripts/index.js";
+import { cena0 } from "./cena0.js";
 
 var cena3 = new Phaser.Scene("Cena 3");
 
 cena3.preload = function () {
-    // Imagem de fundo
-    this.load.image("perdeu", "../assets/gameovertela.png");
+  // Imagem de fundo
+  this.load.image("perdeu", "../assets/gameovertela.png");
 };
 
-iniciar.on(
-    "pointerdown",
-    function () {
-        this.scene.start(cena0);
-    },
-    this
-);
+cena3.create = function () {
+   var button = this.add.image(400, 300, "perdeu", 0).setInteractive();
+   button.on(
+     "pointerdown",
+     function () {
+       this.scene.start(cena0);
+     },
+     this
+   );
+};
 
-home.on(
-    "pointerdown",
-    function () {
-        this.scene.start(cena0);
-    },
-    this
-);
-
-
-cena3.update = function () { };
+cena3.update = function () {};
 
 // Exportar a cena
 export { cena3 };
