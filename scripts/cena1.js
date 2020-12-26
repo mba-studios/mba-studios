9b1728db8ba362f5d1f17a894994ef6ad26d08b0
Cadore-ac <arthur.cmb2020@gmail.com>
2020-12-26 05:22:46 +0000

    26/12

--- scripts/cena1.js
+++ scripts/cena1.js
@@ -1,5 +1,14 @@
+/*
+File: cena1.html
+Author: Arthur Cadore(IFSC)
+Date: 26 / 12 / 2020
+Brief: second import archive.
+*/
+
+// [Arthur] importando a cena seguinte do arquivo "cena2.js"
 import { cena2 } from "./cena2.js";
 
+// [Arthur] Adicionando variáveis ao código para execução dos comandos. 
 var player;
 var player2;
 var platforms;
@@ -15,45 +24,44 @@ var lifeText;
 var timedEvent;
 var timer;
 var timerText;
-
-
-
 var cena1 = new Phaser.Scene("Cena 1");
 
-
+// ===============================================================================
+// [Arthur] Iniciando a função Preload, para carregar os arquivos iniciais da cena. 
 cena1.preload = function () {
+
+    // [Arthur] Carregando imagens para a cena.
     this.load.image("sky", "../assets/plano de fundo.png");
     this.load.image("ground", "../assets/plataformagelo.png");
     this.load.image("wall", "../assets/plataformagelovertical.png");
     this.load.image("slab", "../assets/plataformagelohorizontalmeiobloco.png");
-
     this.load.image("bomb", "../assets/bomb.png");
 
-this.load.audio("musica", "../assets/musica0.mp3");
+
+
+    // [Arthur] Carregando audios para a cena. 
+    this.load.audio("musica", "../assets/musica0.mp3");
     this.load.audio("pular", "../assets/pular.mp3");
 
+
+
+    // [Arthur] Carregando sprites para a cena. 
     this.load.spritesheet("dude", "../assets/dino1.png", {
         frameWidth: 24,
         frameHeight: 24,
-    });
-
-    this.load.spritesheet("water", "../assets/liquido.agua.png", {
-        frameWidth: 128,
-        frameHeight: 16,
     });
     this.load.spritesheet("dude2", "../assets/dino1.png", {
         frameWidth: 24,
         frameHeight: 24,
     });
-
+    this.load.spritesheet("water", "../assets/liquido.agua.png", {
+        frameWidth: 128,
+        frameHeight: 16,
+    });
     this.load.spritesheet("gate", "../assets/Portal.agua1.png", {
         frameWidth: 64,
         frameHeight: 64,
     });
-
-    
-
-    // Tela cheia
     this.load.spritesheet("fullscreen", "../assets/fullscreen.png", {
         frameWidth: 64,
         frameHeight: 64,
@@ -61,58 +69,69 @@ this.load.audio("musica", "../assets/musica0.mp3");
 
 };
 
+
+// ===============================================================================
+// [Arthur] Iniciando a função create, para montar os componentes da cena. 
 cena1.create = function () {
 
-    trilha = this.sound.add("musica"); // (comentar com o professor, demora muito pra carregar, deixei desabilitado)
+    // [Arthur] Incluido o som da musica a cena, e em seguida dando play. 
+    trilha = this.sound.add("musica");
     trilha.play();
 
-    pular = this.sound.add("pular"); // (comentar com o professor, demora muito pra carregar, deixei desabilitado)
+    // [Arthur] Incluindo os sons de efeito a cena. 
+    pular = this.sound.add("pular");
 
 
+    // [Arthur] Incluindo a imagem de fundo a cena. 
     this.add.image(400, 300, "sky");
 
-    gate = this.physics.add.sprite(736, 120, "gate");
 
+    // [Arthur] Incluido a sprite do portal a cena e em seguida, criando a animação. 
+    gate = this.physics.add.sprite(736, 120, "gate");
     this.anims.create({
         key: "gateon",
         frames: this.anims.generateFrameNumbers("gate", { start: 1, end: 4 }),
         frameRate: 10,
         repeat: -1,
     });
-
     gate.anims.play("gateon");
 
+    // [Arthur] Incluindo a sprite de água a cena e em seguinda, criando a animação. 
     water = this.physics.add.sprite(222, 382, "water");
-    water2 = this.physics.add.sprite(382, 574, "water");
-    water3 = this.physics.add.sprite(572, 254, "water");
-
     this.anims.create({
         key: "wateron",
         frames: this.anims.generateFrameNumbers("water", { start: 1, end: 4 }),
         frameRate: 10,
         repeat: -1,
     });
+    water.anims.play("wateron");
 
+    // [Arthur] Incluindo a sprite de água a cena e em seguinda, criando a animação.
+    water2 = this.physics.add.sprite(382, 574, "water");
     this.anims.create({
         key: "wateron",
         frames: this.anims.generateFrameNumbers("water2", { start: 1, end: 4 }),
         frameRate: 10,
         repeat: -1,
     });
+    water2.anims.play("wateron");
 
+    // [Arthur] Incluindo a sprite de água a cena e em seguinda, criando a animação.
+    water3 = this.physics.add.sprite(572, 254, "water");
     this.anims.create({
         key: "wateron",
         frames: this.anims.generateFrameNumbers("water3", { start: 1, end: 4 }),
         frameRate: 10,
         repeat: -1,
     });
-
-    water.anims.play("wateron");
-    water2.anims.play("wateron");
     water3.anims.play("wateron");
+
+    // ================================================
+    // [Arthur] Incluindo o grupo de plataformas a cena, grupo definido como estático. 
 
     platforms = this.physics.add.staticGroup();
 
+    // [Arthur] Incluindo grupo de plataformas do piso. 
     platforms.create(64, 582, "ground");
     platforms.create(256, 582, "ground");
     platforms.create(192, 582, "ground");
@@ -121,6 +140,8 @@ cena1.create = function () {
     platforms.create(640, 582, "ground");
     platforms.create(768, 582, "ground");
 
+
+    // [Arthur] Incluindo grupo de plataformas do teto.
     platforms.create(64, 8, "ground");
     platforms.create(256, 8, "ground");
     platforms.create(192, 8, "ground");
@@ -129,99 +150,121 @@ cena1.create = function () {
     platforms.create(640, 8, "ground");
     platforms.create(768, 8, "ground");
 
+
+    // [Arthur] Incluindo grupo de plataformas do 1º andar. 
     platforms.create(96, 486, "ground");
     platforms.create(224, 486, "ground");
     platforms.create(704, 486, "ground");
 
+
+    // [Arthur] Incluindo grupo de plataformas do 2º andar. 
     platforms.create(96, 390, "ground");
     platforms.create(224, 398, "slab");
     platforms.create(352, 390, "ground");
     platforms.create(480, 390, "ground");
     platforms.create(544, 390, "ground");
 
+
+    // [Arthur] Incluindo grupo de plataformas do 3º andar. 
     platforms.create(32, 294, "ground");
 
+
+    // [Arthur] Incluindo grupo de plataformas do 4º andar. 
     platforms.create(704, 262, "ground");
     platforms.create(576, 270, "slab");
     platforms.create(448, 262, "ground");
     platforms.create(352, 230, "ground");
     platforms.create(224, 230, "ground");
 
+
+    // [Arthur] Incluindo grupo de plataformas do 5º andar. 
     platforms.create(704, 166, "ground");
 
+
+    // [Arthur] Incluindo grupo de plataformas da parede esquerda. 
     platforms.create(16, 536, "wall");
     platforms.create(16, 408, "wall");
     platforms.create(16, 280, "wall");
     platforms.create(16, 152, "wall");
     platforms.create(16, 24, "wall");
 
+
+    // [Arthur] Incluindo grupo de plataformas da parede direita. 
     platforms.create(784, 536, "wall");
     platforms.create(784, 408, "wall");
     platforms.create(784, 280, "wall");
     platforms.create(784, 152, "wall");
     platforms.create(784, 24, "wall");
 
-    player = this.physics.add.sprite(128, 500, "dude");
+    // ================================================
+    // [Arthur] Incluindo grupo de sprites a cena. 
 
+    // [Arthur] Incluindo sprites do player Nº 1. 
+    player = this.physics.add.sprite(128, 500, "dude");
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
 
+
+    // [Arthur] Incluindo as funções de movimentação do personagem Nº1.
     this.anims.create({
         key: "left",
         frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
         frameRate: 10,
         repeat: -1,
     });
-
-    this.anims.create({
-        key: "turn",
-        frames: [{ key: "dude", frame: 1 }],
-        frameRate: 20,
-    });
-
     this.anims.create({
         key: "right",
         frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
         frameRate: 10,
         repeat: -1,
     });
+    this.anims.create({
+        key: "turn",
+        frames: [{ key: "dude", frame: 1 }],
+        frameRate: 20,
+    });
 
+
+    // [Arthur] Incluindo sprites do player Nº 1. 
     player2 = this.physics.add.sprite(128, 400, "dude2");
-
     player2.setBounce(0.2);
     player2.setCollideWorldBounds(true);
 
+
+    // [Arthur] Incluindo as funções de movimentação do personagem Nº2. 
     this.anims.create({
         key: "left2",
         frames: this.anims.generateFrameNumbers("dude2", { start: 5, end: 8 }),
         frameRate: 10,
         repeat: -1,
     });
-
-    this.anims.create({
-        key: "turn2",
-        frames: [{ key: "dude2", frame: 1 }],
-        frameRate: 20,
-    });
-
     this.anims.create({
         key: "right2",
         frames: this.anims.generateFrameNumbers("dude2", { start: 5, end: 8 }),
         frameRate: 10,
         repeat: -1,
     });
+    this.anims.create({
+        key: "turn2",
+        frames: [{ key: "dude2", frame: 1 }],
+        frameRate: 20,
+    });
 
+
+    // [Arthur] Igualando as funções de sprite aos cursores da funçao seguinte. 
     cursors = this.input.keyboard.createCursorKeys();
 
 
+    // [Arthur] Adicionando sistema de colisão a fase. 
     this.physics.add.collider(player, platforms);
+    this.physics.add.collider(player2, platforms);
     this.physics.add.collider(water, platforms);
     this.physics.add.collider(water2, platforms);
     this.physics.add.collider(water3, platforms);
     this.physics.add.collider(gate, platforms);
-    this.physics.add.collider(player2, platforms);
 
 
+    // [Arthur] Incluindo movimentação dos personagens e ativando a função dos sprites. 
     this.input.keyboard.on(
         "keydown_A",
         function () {
@@ -230,7 +273,6 @@ cena1.create = function () {
         },
         this
     );
-
     this.input.keyboard.on(
         "keyup_A",
         function () {
@@ -239,7 +281,6 @@ cena1.create = function () {
         },
         this
     );
-
     this.input.keyboard.on(
         "keydown_D",
         function () {
@@ -248,7 +289,6 @@ cena1.create = function () {
         },
         this
     );
-
     this.input.keyboard.on(
         "keyup_D",
         function () {
@@ -257,37 +297,36 @@ cena1.create = function () {
         },
         this
     );
-
     this.input.keyboard.on(
         "keydown_W",
         function () {
             if (player2.body.touching.down == 1) {
                 player2.setVelocityY(-300);
-
-
-                pular.play(); //ver com o professor, tela fica preta
+                pular.play();
             }
         },
         this
     );
 
+
+    // [Arthur] Função para avançar a cena, posteriormente será trocada por um evento de colisão. 
     this.input.keyboard.on(
         "keydown_X",
         function () {
-            trilha.stop(); //botão para de funcionar
+            trilha.stop();
             this.scene.start(cena2);
         },
         this
     );
 
-    // Botão de ativar/desativar tela cheia
+
+
+    // [Arthur] Adicionando a função de ligar / desligar a tela cheia. 
     var button = this.add
         .image(800 - 16, 16, "fullscreen", 0)
         .setOrigin(1, 0)
         .setInteractive()
         .setScrollFactor(0);
-
-    // Ao clicar no botão de tela cheia
     button.on(
         "pointerup",
         function () {
@@ -302,7 +341,8 @@ cena1.create = function () {
         this
     );
 
-    // Tecla "F" também ativa/desativa tela cheia
+
+    // [Arthur] Adiconando a função de tela cheia a partir da tecla F. 
     var FKey = this.input.keyboard.addKey("F");
     FKey.on(
         "down",
@@ -317,14 +357,14 @@ cena1.create = function () {
         },
         this
     );
-    
-   lifeText = this.add.text(20, 24, life, {
+
+
+    // [Arthur] Adicionando contador a fase do jogo. 
+    lifeText = this.add.text(20, 24, life, {
         fontSize: "32px",
         fill: "#cccccc",
     });
     lifeText.setScrollFactor(0);
-
-    // Mostra na tela o contador
     timerText = this.add.text(16, 16, timer, {
         fontSize: "32px",
         fill: "#000",
@@ -334,42 +374,41 @@ cena1.create = function () {
 
 };
 
+
+// ================================================
+// [Arthur] Iniciando a função update a cena, a função é executada em loop para algumas mecânicas da fase. 
 cena1.update = function () {
+
+
+    // [Arthur] Incluindo as funções de movimentação do personagem Nº1. 
     if (cursors.left.isDown) {
         player.setVelocityX(-160);
-
         player.anims.play("left", true);
     } else if (cursors.right.isDown) {
         player.setVelocityX(160);
-
         player.anims.play("right", true);
     } else {
         player.setVelocityX(0);
-
         player.anims.play("turn");
     }
-
     if (cursors.up.isDown && player.body.touching.down) {
         player.setVelocityY(-300);
-
-       pular.play(); //ver com o professor tela fica preta
+        pular.play();
     }
-
 };
+
+
+// ================================================
+// [Arthur] Iniciando a função contdown, para fazer a contagem do tempo da cena. 
 function countdown() {
-    // Adiciona o tempo de vida em 1 segundo
     life += 1;
     lifeText.setText(life);
-    // Reduz o contador em 1 segundo
     timer -= 1;
     timerText.setText(timer);
-    // Se o contador chegar a zero, inicia a cena 2
     if (timer === 0) {
         trilha.stop();
         this.scene.start(cena2);
     }
-
-
 }
-
+// [Arthur] Exportando cena 2 para o index.js.
 export { cena1 };
