f99c915788297725dbae4318ec77e3e5519f2e6f
Cadore-ac <arthur.cmb2020@gmail.com>
2021-02-28 22:44:17 +0000

    documentacao

--- servidor/servidor.js
+++ servidor/servidor.js
@@ -1,6 +1,16 @@
+/*
+File: cena0.js
+Author: Arthur Cadore(IFSC)
+Date: 28 / 02 / 2021 
+*/
+
+// [Arthur]
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
 
+// [Arthur] Define o número da porta a utilizar para abrir o servidor 
+const PORT = process.env.PORT || 4000;
+
+// [Arthur] Define o diretório á ser importado 
 app.use(express.static("../cliente"));
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
